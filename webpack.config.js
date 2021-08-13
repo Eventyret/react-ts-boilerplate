dc40e9c0af46b95a71354252f7c9cf8ea4095388
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-13 21:49:49 +0100

    Finished simple webpack and installed cli and ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
 const path = require('path');
 module.exports = {
   module: 'developmen',
-  entry: './src/test.tsx'
+  entry: './src/test.tsx',
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        test: /\.tsx?$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
