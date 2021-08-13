7b3e66116fbbadd5a56f0e44a412733e6b4dd192
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-13 22:06:23 +0100

    Default render

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,3 +1,7 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 const test = <div>Hello world</div>;
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+ReactDOM.render(test, root);
